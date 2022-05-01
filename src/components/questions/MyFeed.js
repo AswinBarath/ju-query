17e0aee90f486d9b4d7557598c38bbd22e71ea42
AswinBarath <aswin2001barath@gmail.com>
2022-05-01 17:19:38 +0530

    Bug Fix
    Changing Questionspage logic with debug console

--- src/components/questions/MyFeed.js
+++ src/components/questions/MyFeed.js
@@ -1,49 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import QueryBox from '../querybox/QueryBox';
-import Post from '../post/Post';
-import db from '../../firebase';
-import './MyFeed.css';
-import { selectUser } from '../../features/userSlice';
-import { useSelector } from 'react-redux';
+import Post from "../post/Post";
+import db from "../../firebase";
+import "./MyFeed.css";
+import { selectUser } from "../../features/userSlice";
+import { useSelector } from "react-redux";
 
 function MyFeed() {
-    const currentUser = useSelector(selectUser);
+  const currentUser = useSelector(selectUser);
 
-    const [posts,setPosts] = useState([])
-    useEffect(()=> {
-        db.collection('questions').orderBy('timestamp','desc').onSnapshot
-        (
-            snapshot => setPosts(
-                snapshot.docs.filter(function (doc) {
-                    // console.log(doc.data().user)
-                    return doc.data().user.email === currentUser.email // || doc.data().user.display === user.display;
-                }).map(function (doc) {
-                    return { 
-                        id:doc.id,
-                        question:doc.data(),
-                    };
-                })
-            )
-        );
-    })
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    db.collection("questions")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs
+            .filter(function (doc) {
+              console.log(
+                doc.data().user,
+                currentUser.email,
+                currentUser.display
+              );
+              if (doc.data().user.email === currentUser.email) {
+                return doc.data().user.email === currentUser.email;
+              } else {
+                return doc.data().user.display === currentUser.display;
+              }
+            })
+            .map(function (doc) {
+              return {
+                id: doc.id,
+                question: doc.data(),
+              };
+            })
+        )
+      );
+  });
 
-    return (
-        <div className="feed">
-            <h2>Your Questions</h2>
-            {
-                posts.map(({id,question})=> (
-                    <Post key={id} 
-                    id={id} 
-                    imageUrl={question.imageUrl} 
-                    question={question.question}
-                    timestamp = {question.timestamp}
-                    JuUser = {question.user}
-                    />
-                ))
-            }
-        </div>
-    )
+  return (
+    <div className="feed">
+      <h2>Your Questions</h2>
+      {posts.map(({ id, question }) => (
+        <Post
+          key={id}
+          id={id}
+          imageUrl={question.imageUrl}
+          question={question.question}
+          timestamp={question.timestamp}
+          JuUser={question.user}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default MyFeed
-
+export default MyFeed;
